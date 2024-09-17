978f27e971669955116cf869c688828d48972f63
Owen Krueger <github@owenkrueger.dev>
2024-09-17 11:17:36 -0500

    Stub out laundry and internet automations

--- src/apps/Internet/Internet.cs
+++ src/apps/Internet/Internet.cs
@@ -1,15 +1,50 @@
-﻿namespace NetDaemon.apps.Internet;
+﻿using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+using HomeAssistantGenerated;
+using NetDaemon.HassModel.Entities;
 
+namespace NetDaemon.apps.Internet;
+
+/// <summary>
+/// Automations for internet.
+/// </summary>
 [NetDaemonApp]
 public class Internet
 {
-    public Internet(IHaContext context)
+    private readonly IEntities entities;
+    private readonly ILogger<Internet> logger;
+    
+    /// <summary>
+    /// SeTs up internet automations.
+    /// </summary>
+    public Internet(IHaContext context, IScheduler scheduler, ILogger<Internet> logger)
     {
+        entities = new Entities(context);
+        this.logger = logger;
 
+        entities.BinarySensor.InternetUp
+            .StateChanges()
+            .WhenStateIsFor(x => x.IsOff(), TimeSpan.FromSeconds(90), scheduler)
+            .SubscribeAsync(async _ => await RestartModemAsync(), e => 
+                logger.LogError(e, "Exception thrown while restarting modem."));
     }
-}
 
-public class InternetConfiguration
-{
-    public SwitchEntity InternetModemSmartPlug { get; set; }
+    /// <summary>
+    /// Attempts to restart the internet modem. This is done by turning off the modem, waiting 15 seconds, and then
+    /// turning the modem back on.
+    /// </summary>
+    private async Task RestartModemAsync()
+    {
+        var modemSmartPlug = entities.Switch.InternetModemSmartPlug;
+        if (modemSmartPlug.EntityState?.LastChanged > DateTime.Now.AddMinutes(-5))
+        {
+            logger.LogInformation("{Entity} already manually restarted. Not restarting", modemSmartPlug.EntityId);
+            return;
+        }
+        
+        logger.LogInformation("Restarting {Entity}.", modemSmartPlug.EntityId);
+        modemSmartPlug.TurnOff();
+        await Task.Delay(TimeSpan.FromSeconds(15));
+        modemSmartPlug.TurnOn();
+    }
 }
---
+++ src/apps/Laundry/Laundry.cs
@@ -0,0 +1,48 @@
+﻿using HomeAssistantGenerated;
+using NetDaemon.Utilities;
+
+namespace NetDaemon.apps.Laundry;
+
+/// <summary>
+/// Automations for laundry.
+/// </summary>
+[NetDaemonApp]
+public class Laundry
+{
+    private readonly IServices services;
+    
+    /// <summary>
+    /// Sets up laundry automations.
+    /// </summary>
+    public Laundry(IHaContext context)
+    {
+        var entities = new Entities(context);
+        services = new Services(context);
+
+        entities.Sensor.WasherWasherMachineState
+            .StateChanges()
+            .Where(x =>
+                x.Old?.State == "run" &&
+                x.New?.State == "stop")
+            .Subscribe(_ => NotifyUsers(true));
+        
+        // Usually, state becomes "finished", but occasionally goes from "cooling" to "none".
+        entities.Sensor.DryerDryerMachineState
+            .StateChanges()
+            .Where(x =>
+                x.Old?.State == "run" &&
+                x.New?.State == "stop")
+            .Subscribe(_ => NotifyUsers(false));
+    }
+
+    /// <summary>
+    /// Notifies users that the washer or dryer has completed.
+    /// </summary>
+    private void NotifyUsers(bool washer)
+    {
+        var device = washer ? "washer" : "dryer";
+        services.Notify.NotifyAll(
+            $"The {device} has completed!",
+            "Laundry");
+    }
+}
