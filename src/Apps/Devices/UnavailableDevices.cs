029469b65e753688ccbbaf9eafeaeb939687bb99
Owen Krueger <github@owenkrueger.dev>
2024-09-25 10:21:57 -0500

    More automations

---
+++ src/Apps/Devices/UnavailableDevices.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using NetDaemon.HassModel.Entities;
+
+namespace NetDaemon.Apps.Devices;
+
+/// <summary>
+/// Automations for pinging unavailable entities to make them active.
+/// </summary>
+[NetDaemonApp]
+public class UnavailableDevices
+{
+    private readonly IScheduler scheduler;
+    private readonly ILogger<UnavailableDevices> logger;
+
+    /// <summary>
+    /// Sets up automations.
+    /// </summary>
+    public UnavailableDevices(IHaContext context, IScheduler scheduler, ILogger<UnavailableDevices> logger)
+    {
+        var entities = new Entities(context);
+        this.scheduler = scheduler;
+        this.logger = logger;
+
+        List<EntityGroup> entityGroups =
+        [
+            new EntityGroup(entities.Switch.AllisonLivingRoomLamp, entities.Button.AllisonLivingRoomLampPing, entities.Switch.OwenLivingRoomLamp),
+            new EntityGroup(entities.Switch.OwenLivingRoomLamp, entities.Button.OwenLivingRoomLampPing, entities.Switch.AllisonLivingRoomLamp),
+            new EntityGroup(entities.Switch.BedroomLights, entities.Button.BedroomLightsPing),
+            new EntityGroup(entities.Switch.CounterLights, entities.Button.CounterLightsPing),
+            new EntityGroup(entities.Switch.DiningRoomLights, entities.Button.DiningRoomLightsPing),
+            new EntityGroup(entities.Switch.FrontPorchLights, entities.Button.FrontPorchLightsPing),
+            new EntityGroup(entities.Switch.DeckStringLights, entities.Button.DeckStringLightsPing),
+            new EntityGroup(entities.Switch.GarageLights, entities.Button.GarageLightsPing),
+            new EntityGroup(entities.Switch.KitchenLights, entities.Button.KitchenLights),
+            new EntityGroup(entities.Switch.LaundryRoomLights, entities.Button.LaundryRoomLightsPing),
+            new EntityGroup(entities.Switch.OfficeLights, entities.Button.OfficeLightsPing),
+            new EntityGroup(entities.Switch.StairwayLights, entities.Button.StairwayLightsPing),
+            new EntityGroup(entities.Switch.UtilityRoomLights, entities.Button.UtilityRoomLightsPing),
+        ];
+
+        foreach (var group in entityGroups)
+        {
+            group.UnavailableEntity
+                .StateChanges()
+                .Where(x => x.New?.State == "unavailable")
+                .Subscribe(_ => PingEntity(group));
+        }
+    }
+
+    /// <summary>
+    /// Pings the entity to try to bring it back.
+    /// </summary>
+    private void PingEntity(EntityGroup group, int count = 1)
+    {
+        logger.LogInformation("Pinging {Entity} because it's unavailable (Attempt {Count})", 
+            group.UnavailableEntity.EntityId, count);
+        
+        group.PingEntity.CallService("button/press");
+        scheduler.Schedule(DateTimeOffset.Now.AddSeconds(10), 
+            _ => VerifyEntityIsAlive(group, count));
+    }
+
+    /// <summary>
+    /// Checks if the entity is back. If it's still unavailable, try again.
+    /// </summary>
+    private void VerifyEntityIsAlive(EntityGroup group, int count)
+    {
+        if (group.PingEntity.State != "unavailable") // Ping fixed state.
+        {
+            SyncEntities(group.UnavailableEntity, group.SyncEntity);
+            return;
+        }
+
+        if (count >= 3) // We were unsuccessful.
+        {
+            logger.LogWarning("{Entity} pinged multiple times but is still unavailable", 
+                group.UnavailableEntity.EntityId);
+            return;
+        }
+
+        // Try to ping again in 5 minutes.
+        scheduler.Schedule(DateTimeOffset.Now.AddMinutes(5),
+            _ => PingEntity(group, ++count));
+    }
+
+    /// <summary>
+    /// Syncs the states between the two entities. 
+    /// </summary>
+    /// <param name="entityToUpdate">Entity to update to match state from <see cref="correctEntity"/></param>
+    /// <param name="correctEntity">Entity to update <see cref="entityToUpdate"/> from.</param>
+    private static void SyncEntities(SwitchEntity entityToUpdate, SwitchEntity? correctEntity)
+    {
+        if (correctEntity is null) // No need to sync entities.
+        {
+            return;
+        }
+
+        var entityToUpdateState = entityToUpdate.IsOn();
+        var correctEntityState = correctEntity.IsOn();
+
+        if (entityToUpdateState == correctEntityState) // Entities already synced.
+        {
+            return;
+        }
+
+        if (correctEntityState)
+        {
+            entityToUpdate.CallService("switch.turn_on");
+            return;
+        }
+
+        entityToUpdate.TurnOff();
+    }
+}
+
+/// <summary>
+/// Group of entities to monitor.
+/// </summary>
+internal record EntityGroup
+{
+    /// <summary>
+    /// Instantiates a new <see cref="EntityGroup"/> without an entity to sync to.
+    /// </summary>
+    public EntityGroup(SwitchEntity unavailableEntity, ButtonEntity pingEntity)
+    {
+        UnavailableEntity = unavailableEntity;
+        PingEntity = pingEntity;
+    }
+
+    /// <summary>
+    /// Instantiates a new <see cref="EntityGroup"/> with an entity to sync to.
+    /// </summary>
+    public EntityGroup(SwitchEntity unavailableEntity, ButtonEntity pingEntity, SwitchEntity syncEntity)
+    {
+        UnavailableEntity = unavailableEntity;
+        PingEntity = pingEntity;
+        SyncEntity = syncEntity;
+    }
+    
+    /// <summary>
+    /// Entity to monitor for state becoming "unavailable".
+    /// </summary>
+    public SwitchEntity UnavailableEntity { get; set; }
+
+    /// <summary>
+    /// Button to press/ping to bring entity back from the dead.
+    /// </summary>
+    public ButtonEntity PingEntity { get; set; }
+
+    /// <summary>
+    /// Optional. Represents the state the <see cref="UnavailableEntity"/> should be set to after becoming
+    /// available again.
+    /// </summary>
+    public SwitchEntity? SyncEntity { get; set; }
+}
---
+++ src/Apps/Lighting/DownstairsSun.cs
@@ -0,0 +1,70 @@
+﻿using System.Reactive.Concurrency;
+
+namespace NetDaemon.apps.Lighting;
+
+/// <summary>
+/// Automations to set downstairs lighting depending on sun elevation.
+/// </summary>
+[NetDaemonApp]
+public class DownstairsSun
+{
+    private readonly IEntities entities;
+    private readonly ILogger<DownstairsSun> logger;
+
+    /// <summary>
+    /// Sets up automations.
+    /// </summary>
+    public DownstairsSun(IHaContext context, IScheduler scheduler, ILogger<DownstairsSun> logger)
+    {
+        entities = new Entities(context);
+        this.logger = logger;
+
+        entities.Light.DownstairsLights
+            .StateChanges()
+            .WhenStateIsFor(x => x?.State == "on", TimeSpan.FromSeconds(5), scheduler)
+            .Subscribe(_ => SetDownstairsLightLevel());
+        // Around sunset
+        entities.Sun.Sun
+            .StateChanges()
+            .Where(x =>
+                int.Parse(x.Old?.State ?? "0") >= 10 &&
+                int.Parse(x.New?.State ?? "0") < 10)
+            .Subscribe(_ => SetDownstairsLightLevel());
+        // Around sunrise
+        entities.Sun.Sun
+            .StateChanges()
+            .Where(x =>
+                int.Parse(x.Old?.State ?? "0") < 10 &&
+                int.Parse(x.New?.State ?? "0") >= 10)
+            .Subscribe(_ => SetDownstairsLightLevel());
+    }
+
+    /// <summary>
+    /// Sets the downstairs light brightness based on the sun elevation.
+    /// </summary>
+    private void SetDownstairsLightLevel()
+    {
+        var brightnessAttribute = entities.Light.DownstairsLights.Attributes?.Brightness;
+        var elevationAttribute = entities.Sun.Sun.Attributes?.Elevation;
+        if (!int.TryParse(brightnessAttribute?.ToString(), out var brightness) ||
+            !int.TryParse(elevationAttribute?.ToString(), out var elevation))
+        {
+            // Unable to pull brightness or elevation.
+            logger.LogError("Unable to parse attributes. Brightness attribute: {Brightness}. Elevation attribute: {Elevation}.",
+                brightnessAttribute, elevationAttribute);
+            return;
+        }
+
+        switch (elevation)
+        {
+            case >= 10 when brightness != 255:
+                logger.LogInformation("Setting downstairs lights to 100% brightness.");
+                entities.Light.DownstairsLights.CallService("light.turn_on", new { brightness = 255 });
+                return;
+            case < 10 when brightness != 128:
+                logger.LogInformation("Setting downstairs to 50% brightness.");
+                entities.Light.DownstairsLights.CallService("light.turn_on", new { brightness = 128 });
+                return;
+        }
+    }
+}
